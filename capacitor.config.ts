03db35e68588a46458a7d42635894633a8495525
ma7801 <ma7801@gmail.com>
2022-08-09 17:26:26 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Piano Quizzer',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
